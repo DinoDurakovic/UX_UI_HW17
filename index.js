436e1f2ff74ce57d2e242464aa8d28bf80b876eb
DinoDurakovic <113859620+DinoDurakovic@users.noreply.github.com>
2023-02-15 17:16:29 -0600

    tried to add hover effect on project tiles

---
+++ index.js
@@ -0,0 +1,7 @@
+$("project1").on("mouseenter", function(){
+    $("project1").css("background-color", "#d9d9d9")
+});
+
+document.getElementById("project1").addEventListener("mouseover", function(){
+    document.getElementById("project1").style.backgroundColor = "grey";
+});
